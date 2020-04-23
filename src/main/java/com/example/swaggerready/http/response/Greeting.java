3036e54f53544ab01422454e0a7ad7df4d44434e
ccaldas <carlos.caldas@agoda.com>
2020-04-23 19:40:01 +0700

    gradle upgraded to java 11

--- src/main/java/com/example/swaggerready/http/response/Greeting.java
+++ src/main/java/com/example/swaggerready/http/response/Greeting.java
@@ -4,10 +4,24 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
 public class Greeting {
 
+  public Greeting(String message) {
+    this.message = message;
+  }
+
+
+
   private String message;
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
 }
